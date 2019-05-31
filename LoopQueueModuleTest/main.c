d48044470a63aa024fef31ac058de8e8bd40ed29
unknown <qbb@LAPTOP-6A0LGM1A>
2019-05-31 23:36:19 +0800

    add test project mul loop queue

---
+++ LoopQueueModuleTest/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "mul_loop_queues.h"
+
+#define PUSH_EQUAL_POP     1
+#define MAX_QUEUE_LENGTH   10
+
+APP_LOOPQUEUE_DEF(Queue,Qmanager,Qlength,MAX_QUEUE_LENGTH);
+
+static void NormalDelay(unsigned int counter)
+{
+	for(; counter > 0; counter --)
+	{
+
+	}
+}
+
+int main(void)
+{
+	X_Boolean isOK;
+	uint8_t buf_number,i;
+	uint8_t data_buf[MAX_QUEUE_LENGTH];
+
+	setbuf(stdout,NULL);
+	queueInitialize(Queue,Qmanager,Qlength);
+	i = 0;
+	while(1)
+	{
+		NormalDelay(0x2fffffff);
+		buf_number = QueueFirstIn(Qmanager,&isOK,X_True);
+		if(isOK == X_True)
+		{
+			data_buf[buf_number] = i;
+			printf("buf_number[%d] pushdata %d  ; occupy permit\r\n",buf_number,i);
+		}
+		else
+		{
+			printf("push failed  i = %d\r\n",i);
+		}
+		i ++;
+
+		buf_number = QueueFirstIn(Qmanager,&isOK,X_False);
+		if(isOK == X_True)
+		{
+			data_buf[buf_number] = i;
+			printf("buf_number[%d] pushdata %d  ; occupy not permit\r\n",buf_number,i);
+		}
+		else
+		{
+			printf("push failed  i = %d\r\n",i);
+		}
+		i ++;
+
+
+		buf_number = QueueFirstOut(Qmanager,&isOK);
+		if(isOK == X_True)
+		{
+			printf("buf_number[%d] popdata %d \r\n",buf_number,data_buf[buf_number]);
+		}
+		else
+		{
+			printf("pop failed \r\n");
+		}
+	}
+}
---
+++ LoopQueueModuleTest/mul_loop_queues.c
@@ -0,0 +1,171 @@
+#include "mul_loop_queues.h"
+
+static X_Boolean ConfigListNode(data_list *dlist)
+{
+	if(dlist == X_Null) {return X_False;}
+
+	dlist->NextPtr = X_Null;
+	dlist->isBufFree = X_True;
+	dlist->isOccupyPermit = X_True;
+	return X_True;
+}
+/*
+static uint8_t GetListLength(data_list *p_list)
+{
+	uint8_t i;
+	for(i=0;i<255;i++)
+	{
+		if(p_list[i] == X_Null )
+		{
+			break;
+		}
+	}
+	if(i<1){i=1;}
+	return (i-1);
+}
+*/
+static X_Void GetListState(List_Manager *p_manager)
+{
+	if(p_manager->FilledBufNum == 0)
+	{
+		p_manager->l_state = empty;
+		return;
+	}
+	if(p_manager->FilledBufNum >= p_manager->ValidNodeNumber)
+	{
+		p_manager->l_state = full;
+		return;
+	}
+	p_manager->l_state = normal;
+}
+X_Void queueInitialize(data_list * p_list,List_Manager *p_manager,uint8_t length)
+{
+	uint8_t i;
+	X_Boolean isOkFlag;
+
+	p_manager->MaxBufNumber = length;
+
+	for(i=0;i<p_manager->MaxBufNumber;i++)
+	{
+
+		isOkFlag = ConfigListNode(&p_list[i]);
+		p_list[i].list_number = i ;
+		if(isOkFlag == X_False)
+		{
+			break;
+		}
+	}
+	p_manager->ValidNodeNumber = i;
+
+	for(i=0;i < p_manager->ValidNodeNumber;i++)
+	{
+		if(i+1 >= p_manager->ValidNodeNumber)
+		{
+			p_list[i].NextPtr = &p_list[0];
+		}
+		else
+		{
+			p_list[i].NextPtr = &p_list[i+1];
+		}
+	}
+	p_manager->FirstIn = &p_list[0];
+	p_manager->FirstOut = &p_list[0];
+	p_manager->l_state = empty;
+	p_manager->FilledBufNum = 0 ;
+}
+
+uint16_t QueueFirstIn(List_Manager *p_manager,X_Boolean *isOK,X_Boolean is_OccupyPermit)
+{
+	uint16_t buf_number;
+	data_list *FI;
+	FI = p_manager->FirstIn;
+	GetListState(p_manager);
+
+	buf_number = 0;
+	switch(p_manager->l_state)
+	{
+		case empty:
+		case normal:
+			if(p_manager->FirstIn ->NextPtr != X_Null){p_manager->FirstIn = p_manager->FirstIn->NextPtr;}
+
+			FI->isBufFree = X_False;
+			FI->isOccupyPermit = is_OccupyPermit;
+			*isOK = X_True;
+			p_manager->FilledBufNum ++;
+			buf_number = FI->list_number;
+		break;
+		case full:
+			if(p_manager->FirstOut->isOccupyPermit == X_True)
+			{
+				if(p_manager->FirstIn ->NextPtr != X_Null){p_manager->FirstIn = p_manager->FirstIn->NextPtr;}
+				else
+				{
+					*isOK = X_False;
+					return 0;
+				}
+				if(p_manager->FirstOut ->NextPtr != X_Null){p_manager->FirstOut = p_manager->FirstOut->NextPtr;}
+				else
+				{
+					*isOK = X_False;
+					return 0;
+				}
+
+				FI->isBufFree = X_False;
+				FI->isOccupyPermit = is_OccupyPermit;
+				*isOK = X_True;
+
+				buf_number = FI->list_number;
+			}
+			else
+			{
+				*isOK = X_False;
+				buf_number = 0;
+			}
+		break;
+		default:
+			*isOK = X_False;
+			buf_number = 0;
+		break;
+	}
+	return buf_number;
+}
+uint16_t QueueFirstOut(List_Manager *p_manager,X_Boolean *isOK)
+{
+	uint16_t buf_number;
+	data_list *FO;
+	FO = p_manager->FirstOut;
+
+	buf_number = 0;
+	GetListState(p_manager);
+	switch(p_manager->l_state)
+	{
+		case empty:
+			*isOK = X_False;
+			buf_number = 0;
+		break;
+		case normal:
+		case full:
+			//
+			if(p_manager->FirstOut ->NextPtr != X_Null){p_manager->FirstOut = p_manager->FirstOut ->NextPtr;}
+			else
+			{
+				*isOK = X_False;
+				return 0;
+			}
+
+			FO->isBufFree = X_True;
+			FO->isOccupyPermit = X_True;
+
+			if(p_manager->FilledBufNum > 0) {p_manager->FilledBufNum --;}
+
+			*isOK = X_True;
+			buf_number = FO->list_number;
+		break;
+		default:
+			*isOK = X_False;
+			buf_number = 0;
+		break;
+	}
+	return buf_number;
+}
+
